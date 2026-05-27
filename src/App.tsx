1bf72652d395e5ca8723ad3d0d37a11645f547a9
Melis <tyn.melis.ru@gmail.com>
2026-05-28 01:03:48 +0600

    Added styles and also got the country when clicking on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { BASE_URL, COUNTRY_LIST } from './consts';
+import { BASE_URL, COUNTRY, COUNTRY_LIST } from './consts';
 
 interface Countries {
   alpha3Code: string;
@@ -8,8 +8,17 @@ interface Countries {
   name: string;
 }
 
+interface Country {
+  name: string;
+  capital: string;
+  population: number;
+  flags: string;
+  borders?: string[];
+}
 const App = () => {
   const [countries, setCountries] = useState<Countries[]>([])
+  const [correctCountry, setCorrectCountry] = useState<string>()
+  const [country, setCountry] = useState<Country>()
 
   useEffect(() => {
     const getCountries = async () => {
@@ -32,11 +41,61 @@ const App = () => {
     getCountries()
   }, [])
 
+  useEffect(() => {
+    if (!correctCountry) return
+    const getCountry = async () => {
+      try {
+        const response = await fetch(BASE_URL + COUNTRY + correctCountry)
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch country')
+        }
+
+        const countryData = await response.json()
+        const { name, capital, population } = countryData
+
+        const borders = countryData.borders?.map((code: string) => {
+          const borderCountry = countries.find(
+            (item) => item.alpha3Code === code,
+          );
+
+          return borderCountry?.name;
+        });
+
+        setCountry(
+          {
+            name,
+            capital,
+            population,
+            flags: countryData.flags.png,
+            borders
+          }
+        )
+      } catch (error) {
+        console.error(error);
+      }
+
+    }
+    getCountry()
+  }, [correctCountry, countries])
+
   return (
-    <div>
-      {countries.map(c => (
-        <div key={c.alpha3Code}>{c.name}</div>
-      ))}
+    <div className='layout'>
+      <aside className='sidebar'>
+        <div className='sidebarHeader'>
+          <h2 className='sidebarTitle'>Countries</h2>
+        </div>
+        <div className='sidebarList'>
+          {countries.map(c => (
+            <div key={c.alpha3Code} className='sidebarItem' onClick={() => setCorrectCountry(c.alpha3Code)}>
+              {c.name}
+            </div>
+          ))}
+        </div>
+      </aside>
+      <main className='main'>
+        <h2>{country?.name}</h2>
+      </main>
     </div>
   )
 }
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
-export const BASE_URL = 'https://restcountries.com/v2/all';
+export const BASE_URL = 'https://restcountries.com';
 
-export const COUNTRY_LIST = '?fields=alpha3Code,name';
+export const COUNTRY_LIST = '/v2/all?fields=alpha3Code,name';
+
+export const COUNTRY = '/v2/alpha/'
